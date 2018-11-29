52901756180ea59710a79e34ab85bec9fb524ded
Harttu Toivonen <harttu.toivonen@raseko.fi>
2018-11-29 10:17:11 +0200

    Kokeilu

---
+++ Assets/Tila.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Luo uusi tila")]
+public class Tila : ScriptableObject {
+    [TextArea(15, 20)] [SerializeField] public string maintext;
+    [SerializeField] Tila[] siirtymäTilat;
+}
---
+++ Assets/peliLogiikka.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class peliLogiikka : MonoBehaviour {
+    [SerializeField] Text tekstiKenttä;
+    [SerializeField] Tila alku;
+    void Start () {
+        tekstiKenttä.text = alku.maintext;
+        Debug.Log("Peli alkoi"); }
+	void Update () {
+		if( Input.GetKeyDown("1") ) {
+            tekstiKenttä.text = "Menit huoneeseen 1";        }
+        if( Input.GetKeyDown("2")) {
+            tekstiKenttä.text = "Menit huoneeseen 2"; }
+        if (Input.GetKeyDown("3")) {
+            tekstiKenttä.text = "Menit huoneeseen 3"; }
+    }
+}
